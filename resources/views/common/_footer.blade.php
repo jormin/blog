8cec54287e4f6b5e49dada49523eab17f1ce673f
Jormin <jorminxee@gmail.com>
2017-06-14 18:46:57 +0800

    调整底部年份

--- resources/views/common/_footer.blade.php
+++ resources/views/common/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer id="footer" class="inner footer reveal text-center">
-    ©&nbsp;2017&nbsp;-&nbsp;Jormin 的博客&nbsp;-&nbsp;
+    ©&nbsp;{{ date('Y') }}&nbsp;-&nbsp;Jormin 的博客&nbsp;-&nbsp;
     <a target="_blank" rel="nofollow" class="external beian" href="http://www.miitbeian.gov.cn/">京ICP备15006639号</a>
 </footer>
 
