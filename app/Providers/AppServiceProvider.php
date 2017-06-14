6a8448d9aae9047bd486878dbfe6254c1961c095
Jormin <jorminxee@gmail.com>
2017-06-14 18:44:02 +0800

    暂时关闭微信分享

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,18 +17,18 @@ class AppServiceProvider extends ServiceProvider
     {
         Carbon::setLocale('zh');
 
-        if (isset($_SERVER['HTTP_HOST'])) {
-            $wx = array(
-                'title' => config("app.name"),
-                'desc' => 'Jormin 的博客',
-                'link' => 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],
-                'imgUrl' => 'https://'.config('filesystems.disks.qiniu.domain').'/logo.png'
-            );
-            View::share('wx',$wx);
-            $app = app('wechat');
-            $wechatjs = $app->js;
-            View::share('wechatjs',$wechatjs);
-        }
+//        if (isset($_SERVER['HTTP_HOST'])) {
+//            $wx = array(
+//                'title' => config("app.name"),
+//                'desc' => 'Jormin 的博客',
+//                'link' => 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],
+//                'imgUrl' => 'https://'.config('filesystems.disks.qiniu.domain').'/logo.png'
+//            );
+//            View::share('wx',$wx);
+//            $app = app('wechat');
+//            $wechatjs = $app->js;
+//            View::share('wechatjs',$wechatjs);
+//        }
     }
 
     /**
